fb9541505298f8eed1f1fbdeb8a3c8053a41ad46
alievgithub <thetomaliev@gmail.com>
2020-09-28 14:12:28 +0300

    Corrected HW

--- 3 семестр/w2/Linux Scripts/absolute.sh
+++ 3 семестр/w2/Linux Scripts/absolute.sh
@@ -4,5 +4,5 @@ cd /tmp/
 mkdir 'hello world'
 cd 'hello world'
 touch absolute.txt
-echo 'hello world!!' > absolute.txt
+echo 'hello world!!' > /tmp/'hello world'/absolute.txt
 cat absolute.txt
--- 3 семестр/w2/Linux Scripts/parent.sh
+++ 3 семестр/w2/Linux Scripts/parent.sh
@@ -4,6 +4,6 @@ cd ..
 mkdir 'hello parent'
 cd 'hello parent'
 touch parent.txt
-echo 'hello parent!!' > parent.txt
+echo 'hello parent!!' > ../'hello parent'/parent.txt
 cat parent.txt
 
