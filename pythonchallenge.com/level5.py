37e9c4dae4df8d43ca3b09c93cd28f9ee2da9ebd
Artem Aliev <artem.aliev@phystech.edu>
2021-12-13 20:07:15 +0300

    Cleaned

--- pythonchallenge.com/level5.py
+++
@@ -1,8 +0,0 @@
-from urllib.request import urlopen
-import pickle
-
-h = urlopen("http://www.pythonchallenge.com/pc/def/banner.p")
-data = pickle.load(h)
-
-for line in data:
-    print("".join([k * v for k, v in line]))
